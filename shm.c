412e96972d05cb91e255dee34c3c03332b84479b
Deven Fafard <dfafa002@sledge.cs.ucr.edu>
2019-12-02 11:50:22 -0800

    Implemented shm_open() and shm_close()

--- shm.c
+++ shm.c
@@ -30,9 +30,50 @@ void shminit() {
 
 int shm_open(int id, char **pointer) {
 
-//you write this
+/* LAB 4 */
 
+acquire(&(shm_table.lock));
 
+unsigned int i= 0;
+unsigned int found = 0;
+
+for(i = 0; i < 64; i++)
+{
+  if(shm_table.shm_pages[i].id == id)
+  {
+    found = 1;
+    break;
+  }
+}
+
+if(!found)
+{
+  for(i = 0; i < 64; i++)
+  {
+    if(shm_table.shm_pages[i].id == 0    && 
+       shm_table.shm_pages[i].frame == 0 &&
+       shm_table.shm_pages[i].refcnt == 0)
+    {
+      break;
+    }
+  }
+
+  char *mem;
+
+  shm_table.shm_pages[i].id = id;
+  mem = kalloc();
+  memset(mem, 0, PGSIZE);
+  shm_table.shm_pages[i].frame = mem;
+}
+
+struct proc *p = myproc();
+
+shm_table.shm_pages[i].refcnt += 1;
+mappages(p->pgdir, (void *)PGROUNDUP(p->sz), PGSIZE, V2P(shm_table.shm_pages[i].frame), PTE_W | PTE_U);
+*pointer = (char *)PGROUNDUP(p->sz);
+p->sz = PGROUNDUP(p->sz) + PGSIZE;
+
+release(&(shm_table.lock));
 
 
 return 0; //added to remove compiler warning -- you should decide what to return
@@ -40,10 +81,30 @@ return 0; //added to remove compiler warning -- you should decide what to return
 
 
 int shm_close(int id) {
-//you write this too!
+/* LAB 4 */
 
+acquire(&(shm_table.lock));
 
+unsigned int i;
 
+for (i = 0; i < 64; i++)
+{
+  if(shm_table.shm_pages[i].id == id)
+  {
+    if(shm_table.shm_pages[i].refnt > 1)
+    {
+      shm_table.shm_pages[i].refcnt -= 1;
+    }
+    else
+    {
+      shm_table.shm_pages[i].id = 0;
+      shm_table.shm_pages[i].frame = 0;
+      shm_table.shm_pages[i].refcnt = 0;
+    }
+  }
+}
+
+release(&(shm_table.lock));
 
 return 0; //added to remove compiler warning -- you should decide what to return
 }
